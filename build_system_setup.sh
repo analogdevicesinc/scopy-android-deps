6f56f6acca26360a5318850540ab0db5596cd0b4
ioanachelaru <Ioana.Chelaru@analog.com>
2021-08-25 14:35:37 +0300

    Fix libusb and jdk version bugs
    Signed-off-by: ioanachelaru <Ioana.Chelaru@analog.com>

--- build_system_setup.sh
+++ build_system_setup.sh
@@ -6,6 +6,6 @@ export ANDROID_SDK_ROOT=$HOME/Android/Sdk
 export CMAKE=$HOME/Qt/Tools/CMake/bin/cmake
 #export CMAKE=/usr/bin/cmake
 export QT_INSTALL_PREFIX=$HOME/Qt/5.15.2/android
-export JDK=/usr/lib/jvm/java-8-openjdk-amd64
+export JDK=$HOME/jdk-16.0.2
 export SCRIPT_HOME_DIR=$HOME/src/scopy-android-deps
 export DEPS_SRC_PATH=$SCRIPT_HOME_DIR/downloads
