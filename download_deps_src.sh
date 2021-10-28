1aaf369092c57d04bf056f5a199f3fb6132a3613
Adrian Suciu <adrian.suciu@analog.com>
2021-10-28 18:00:35 +0300

    Changed jdk download link
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- download_deps_src.sh
+++ download_deps_src.sh
@@ -17,8 +17,8 @@ download_deps() {
 
 install_jdk() {
 	cd $HOME
-	wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/jdk-16.0.2_linux-x64_bin.tar.gz
-	tar xvf jdk-16.0.2_linux-x64_bin.tar.gz
+	wget https://download.java.net/openjdk/jdk16/ri/openjdk-16+36_linux-x64_bin.tar.gz
+	tar xvf openjdk-16+36_linux-x64_bin.tar.gz
 }
 
 download_deps
