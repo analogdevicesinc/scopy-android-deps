5ece37487535c5fbef879fe648ec96063b11e729
Adrian Suciu <adrian.suciu@analog.com>
2021-10-29 14:49:56 +0300

    Added Dockerfile
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- download_deps_src.sh
+++ download_deps_src.sh
@@ -17,7 +17,7 @@ download_deps() {
 
 install_jdk() {
 	cd $HOME
-	sudo apt-get install openjdk-17-jdk
+	sudo apt-get -y install openjdk-17-jdk
 #	wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/jdk-16.0.2_linux-x64_bin.tar.gz
 #	tar xvf jdk-16.0.2_linux-x64_bin.tar.gz
 }
