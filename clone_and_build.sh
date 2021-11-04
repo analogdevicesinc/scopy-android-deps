aab054ca489147dc835c735fad7c7c851895bbba
Adrian Suciu <adrian.suciu@analog.com>
2021-11-04 19:38:54 +0200

    Use master branch for clone_and_build.sh
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- clone_and_build.sh
+++ clone_and_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export BRANCH=androiddecoders
-wget https://raw.githubusercontent.com/analogdevicesinc/scopy/$BRANCH/CI/appveyor/build_scopy_apk.sh
+wget https://raw.githubusercontent.com/analogdevicesinc/scopy/master/CI/appveyor/build_scopy_apk.sh
 chmod +x ./build_scopy_apk.sh
 ./build_scopy_apk.sh
