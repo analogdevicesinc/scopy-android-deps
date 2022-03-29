bf27081c28d203046a8e163d11b1e5ae73b08492
Adrian Suciu <adrian.suciu@analog.com>
2022-03-29 17:06:18 +0300

    Patch to support signing and release
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- rename_gradle.sh
+++ rename_gradle.sh
@@ -3,8 +3,24 @@ set -xe
 source ./build_system_setup.sh $1
 
 rename_gradle() {
-        cd $QT_INSTALL_PREFIX/src/3rdparty/gradle/gradle/wrapper
-        sed -i "s|https\\\:\/\/services\.gradle\.org\/distributions\/gradle.*\.zip|https\://services.gradle.org/distributions/gradle-6.3-all.zip|g" gradle-wrapper.properties
+	echo "Patching to use gradle-6.3"
+
+	pushd $QT_INSTALL_PREFIX/src/3rdparty/gradle/gradle/wrapper
+	sed -i "s|https\\\:\/\/services\.gradle\.org\/distributions\/gradle.*\.zip|https\://services.gradle.org/distributions/gradle-6.3-all.zip|g" gradle-wrapper.properties
+	popd
+}
+
+patch_sdk_build_tools_revision() {
+	echo "Patching qt android to support sdkBuildToolsRevision"
+	# https://bugreports.qt.io/browse/QTBUG-84302
+
+	pushd $QT_INSTALL_PREFIX/lib/cmake/Qt5Core/
+	cp Qt5AndroidSupport.cmake Qt5AndroidSupport.cmake.backup
+	sed '59 a "sdkBuildToolsRevision": "@ANDROID_SDK_BUILD_TOOLS_REVISION@",' Qt5AndroidSupport.cmake.backup  > Qt5AndroidSupport.cmake
+	rm -rf Qt5AndroidSupport.cmake.backup
+	popd
 }
 
 rename_gradle
+patch_sdk_build_tools_revision
+
